29baf7704e83d51db0c8cdb70a1b5dd6341fc12e
sergioJM05 <sergiogino8@hotmail.com>
2025-12-05 11:24:18 -0500

    feat: add alerts contracts

---
+++ src/main/java/com/restock/platform/resource/application/internal/queryservices/AlertQueryServiceImpl.java
@@ -0,0 +1,7 @@
+package com.restock.platform.resource.application.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlertQueryServiceImpl implements AlertQueryService{
+}
