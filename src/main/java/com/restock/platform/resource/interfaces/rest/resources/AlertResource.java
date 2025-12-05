090215172e8128156424bcee90beb96b7db8d17e
sergioJM05 <sergiogino8@hotmail.com>
2025-12-05 11:35:55 -0500

    feat: add alert resource -v2

--- src/main/java/com/restock/platform/resource/interfaces/rest/resources/AlertResource.java
+++ src/main/java/com/restock/platform/resource/interfaces/rest/resources/AlertResource.java
@@ -1,4 +1,10 @@
 package com.restock.platform.resource.interfaces.rest.resources;
 
-public record AlertResource() {
-}
+public record AlertResource(
+        Long id,
+        String message,
+        Long orderId,
+        String orderSituation,
+        Long supplierId,
+        Long recipientId
+) {}
