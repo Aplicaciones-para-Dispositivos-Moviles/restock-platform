424f6da49f75c5a2ca897169627bf5c7b4903065
sergioJM05 <sergiogino8@hotmail.com>
2025-12-05 23:16:24 -0500

    fix: change alert status with order status

--- src/main/java/com/restock/platform/resource/domain/model/entities/Alert.java
+++ src/main/java/com/restock/platform/resource/domain/model/entities/Alert.java
@@ -5,11 +5,13 @@ import com.restock.platform.resource.domain.model.valueobjects.OrderToSupplierSi
 import com.restock.platform.resource.domain.model.valueobjects.OrderToSupplierState;
 import com.restock.platform.shared.domain.model.entities.AuditableModel;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 @Document(collection = "alerts")
 public class Alert extends AuditableModel {
 
